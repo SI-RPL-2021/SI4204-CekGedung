edc14c266ad8dc50e8eac6d94ae32772bfce2df1
Syafiq Dzaky Abhiyasa <dzakyabhiyasa@gmail.com>
2021-05-23 23:18:58 +0700

    menambahkan approval

--- resources/views/layouts/_app.blade.php
+++ resources/views/layouts/_app.blade.php
@@ -111,7 +111,7 @@
             <div class="scroll-sidebar">
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav">
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark" href="{{ route('index') }}" aria-expanded="false"><i class="icon-File-Hide"></i><span class="hide-menu">Starter page </span></a></li>
+                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark" href="{{ route('index') }}" aria-expanded="false"><i class="icon-File-Hide"></i><span class="hide-menu">List Gedung</span></a></li>
                         <li class="nav-small-cap"><i class="mdi mdi-dots-horizontal"></i> <span class="hide-menu">Personal</span></li>
                         <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="icon-Car-Wheel"></i><span class="hide-menu">Dashboard </span></a>
                             <ul aria-expanded="false" class="collapse  first-level">
